9b2f34f794677e6a88312585d3d578bf1126b1e6
Manuel De Stefano <m.destefano36@studenti.unisa.it>
2019-06-13 16:29:57 +0200

    Single house query created

--- gotwiki/houses/routes.py
+++ gotwiki/houses/routes.py
@@ -1,6 +1,13 @@
 from . import houses
-from flask import render_template
+from flask import render_template,jsonify
+from .models import HouseModel,House
 
 @houses.route('/')
 def index():
     return render_template('houses/index.html')
+
+@houses.route('/<houseName>')
+def view_house(houseName):
+    model = HouseModel(houseName)
+    house = model.getHouse()
+    return house.__str__()
